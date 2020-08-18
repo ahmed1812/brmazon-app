e6047bff7b7bbb017633fb545941f3a34d895213
ahmed ahmed <algandel19@gmail.com>
2020-08-17 21:29:52 -0500

    userRoute

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import User from '../models/userModels'
+
+const router = express.Router();
+
+router.get("/createadmin", async(req,res) => {
+    try{
+        const user = new User({
+            name: 'ahmed',
+            email: 'algandel16@gmail.com',
+            password: '1234',
+            isAdmin: true
+        })
+    
+        const newUser = await user.save();
+        res.send(user);
+    } catch(error){
+         res.send({ msg: error.message})
+    }
+});
+
+
+export default router
